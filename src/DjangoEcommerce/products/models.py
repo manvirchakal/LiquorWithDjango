6e7f8d640b3ef8520ea6027784adf2737d9ddbdf
manvirchakal <mchakal31@gmail.com>
2022-01-11 17:41:56 -0600

    added new model fields to app products

--- src/DjangoEcommerce/products/models.py
+++ src/DjangoEcommerce/products/models.py
@@ -3,4 +3,11 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
 	name 		= models.CharField(max_length=200)
-	price 		= models.DecimalField(max_digits=10000,decimal_places=2)
+	capacity 	= models.IntegerField(default=0)
+	price 		= models.DecimalField(max_digits=10000,decimal_places=2,default=0.00)
+	amount 		= models.IntegerField(default=0)
+	cost 		= models.DecimalField(max_digits=10000,decimal_places=2,default=0.00)
+	category 	= models.CharField(max_length=50,default='Uncategorized')
+	featured 	= models.BooleanField(default=False)
+
+	
