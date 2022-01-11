6da86253c780684fff4fc3fbb6b4e92db396c8e2
manvirchakal <mchakal31@gmail.com>
2022-01-11 17:30:44 -0600

    had files that had been modified nothing major

--- src/DjangoEcommerce/products/models.py
+++ src/DjangoEcommerce/products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Product():
+class Product(models.Model):
 	name 		= models.CharField(max_length=200)
+	price 		= models.DecimalField(max_digits=10000,decimal_places=2)
