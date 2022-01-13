5c4c558c2e4898f8e6fd2b7644e6ae6015d69987
manvirchakal <mchakal31@gmail.com>
2022-01-13 15:52:25 -0600

    i think the problem was the template i was using time to try a differnet one

--- src/DjangoEcommerce/pages/views.py
+++ src/DjangoEcommerce/pages/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def homepage_view(request, *args, **kwargs):
-	return render(request, "index.html", {})
+	return render(request, "base.html", {})
