21ff6c49573f7be4609b809cfe5e1f1d9ea23ba1
manvirchakal <mchakal31@gmail.com>
2022-01-15 14:39:52 -0600

    forgot to add new apps in settings.py

--- src/DjangoEcommerce/users/models.py
+++ src/DjangoEcommerce/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
+class RegisteredUser(models.Model):
 	name 		= models.CharField(max_length=100)
 	email 		= models.CharField(max_length=200)
 	phone 		= models.IntegerField()
