2fb064a47f276b842b07ce15af3249b54ae17457
manvirchakal <mchakal31@gmail.com>
2022-01-14 19:13:58 -0600

    added tags inti home.html to dynamically render products in backend

--- src/DjangoEcommerce/pages/views.py
+++ src/DjangoEcommerce/pages/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 def homepage_view(request, *args, **kwargs):
-	return render(request, "index.html", {})
+	featured_items = Product.objects.filter(featured=True)
+	count = 1
+	context = {'featured_items': featured_items}
+
+	return render(request, "index.html", context)
--- src/DjangoEcommerce/products/models.py
+++ src/DjangoEcommerce/products/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
 	name 		= models.CharField(max_length=200)
-	capacity 	= models.IntegerField(default=0)
-	price 		= models.DecimalField(max_digits=10000,decimal_places=2,default=0.00)
-	amount 		= models.IntegerField(default=0)
-	cost 		= models.DecimalField(max_digits=10000,decimal_places=2,default=0.00)
+	capacity 	= models.IntegerField()
+	price 		= models.DecimalField(max_digits=10000,decimal_places=2)
+	amount 		= models.IntegerField()
+	cost 		= models.DecimalField(max_digits=10000,decimal_places=2)
 	category 	= models.CharField(max_length=50,default='Uncategorized')
 	featured 	= models.BooleanField(default=False)
 	image 		= models.ImageField(null=True, blank=True, upload_to="products/images/")
