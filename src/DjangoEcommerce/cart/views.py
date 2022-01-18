0e00982595277228056f69b680b05dcc62e453cb
manvirchakal <mchakal31@gmail.com>
2022-01-17 23:12:16 -0600

    Got the add to cart function working and set up the remove from cart function but ran into some problems trying to implement it

--- src/DjangoEcommerce/cart/views.py
+++ src/DjangoEcommerce/cart/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
+from .models import OrderProduct, Order
+from products.models import Product
+
+def add_to_cart(request, my_id):
+	product = get_object_or_404(Product, id=my_id)
+	order_product, created = OrderProduct.objects.get_or_create(user=request.user,item=product,ordered=False)
+	order_check = Order.objects.filter(user=request.user,ordered=False)
+
+	if order_check.exists():
+		order = order_check[0]
+
+		if order.products.filter(item=product).exists():
+			order_product.quantity += 1
+			order_product.save()
+			print("Added quantity")
+			return redirect("http://127.0.0.1:8000/") 
+
+		else:
+			order.products.add(order_product)
+			print("Product added to cart")
+			return redirect("http://127.0.0.1:8000/")
+
+	else:
+		order = Order.objects.create(user=request.user)
+		order.products.add(order_product)
+		print("Product added to cart")
+		return redirect("http://127.0.0.1:8000/")
+
+def remove_from_cart(request, my_id):
+	product = get_object_or_404(Product, id=my_id)
+	order_check = Order.objects.filter(user=request.user,ordered=False)
+
+	if order_check.exists():
+		order = order_check[0]
+
+		if order.products.filter(item=product).exists():
+			order_product = OrderProduct.objects.filter(item=product,user=request.user,ordered=False)[0]
+			order_product.delete()
+			print("Item has been removed from your cart")
+			return redirect("http://127.0.0.1:8000/")
+
+		else:
+			print("This item is not in your cart")
+			return redirect("http://127.0.0.1:8000/")
+
+	else:
+		print("You do not have an order")
+		return redirect("http://127.0.0.1:8000/")
--- src/DjangoEcommerce/products/views.py
+++ src/DjangoEcommerce/products/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Product
+
+def dynamic_lookup_view(request, my_id):
+	product = Product.objects.get(id=my_id)
+	context = {
+		"product": product
+	}
+	return render(request, "products/product_detail.html", context)
