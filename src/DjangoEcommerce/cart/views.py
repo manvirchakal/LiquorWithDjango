4adfd358827f9fef4b71ef0c2fd9ca791822e706
manvirchakal <mchakal31@gmail.com>
2022-01-21 20:23:28 -0600

    upgraded the django framework to django4 so that it works with newer versions of python and also ended up spotting and fixing a problem where anonymous users were unable to view any pages but solved it by redirecting them to the signup page whenever they tried view or add to the cart

--- src/DjangoEcommerce/cart/views.py
+++ src/DjangoEcommerce/cart/views.py
@@ -5,30 +5,34 @@ from .models import OrderProduct, Order
 from products.models import Product
 
 def add_to_cart(request, my_id):
-	product = get_object_or_404(Product, id=my_id)
-	order_product, created = OrderProduct.objects.get_or_create(user=request.user,item=product,ordered=False)
-	order_check = Order.objects.filter(user=request.user,ordered=False)
-	print(request.META.get('HTTP_REFERER'))
+	if request.user.is_authenticated:
+		product = get_object_or_404(Product, id=my_id)
+		order_product, created = OrderProduct.objects.get_or_create(user=request.user,item=product,ordered=False)
+		order_check = Order.objects.filter(user=request.user,ordered=False)
+		print(request.META.get('HTTP_REFERER'))
 
-	if order_check.exists():
-		order = order_check[0]
+		if order_check.exists():
+			order = order_check[0]
 
-		if order.products.filter(item=product).exists():
-			order_product.quantity += 1
-			order_product.save()
-			print("Added quantity")
-			return redirect(request.META.get('HTTP_REFERER')) 
+			if order.products.filter(item=product).exists():
+				order_product.quantity += 1
+				order_product.save()
+				print("Added quantity")
+				return redirect(request.META.get('HTTP_REFERER')) 
+
+			else:
+				order.products.add(order_product)
+				print("Product added to cart")
+				return redirect(request.META.get('HTTP_REFERER'))
 
 		else:
+			order = Order.objects.create(user=request.user)
 			order.products.add(order_product)
 			print("Product added to cart")
 			return redirect(request.META.get('HTTP_REFERER'))
 
 	else:
-		order = Order.objects.create(user=request.user)
-		order.products.add(order_product)
-		print("Product added to cart")
-		return redirect(request.META.get('HTTP_REFERER'))
+		return redirect('/signup')
 
 def remove_from_cart(request, my_id):
 	product = get_object_or_404(Product, id=my_id)
--- src/DjangoEcommerce/pages/views.py
+++ src/DjangoEcommerce/pages/views.py
@@ -1,29 +1,42 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from products.models import Product, Category, Capacity
 from cart.models import OrderProduct, Order
 
 # Create your views here.
 def homepage_view(request, *args, **kwargs):
 	#print(request.user)
-	order = Order.objects.filter(user=request.user, ordered=False)
-	#print(cart[0].products)
-	order_items = order[0].products.all()
-	#print(type(order_items[0]))
-	'''print(order_items)
-				items = order_items[0].item
-				print(items)'''
-	featured_items = Product.objects.filter(featured=True)
-	categories = Category.objects.all()
-	#capacities = Capacity.objects.all()
-	length = len(order_items)
-	context = {
-		'order_items': order_items,
-		'order': order,
-		'length': length,
-		'featured_items': featured_items,
-		'categories': categories,
-		#'capacities': capacities
+	if request.user.is_authenticated:
+		order = Order.objects.filter(user=request.user, ordered=False)
+		#print(cart[0].products)
+		order_items = order[0].products.all()
+		#print(type(order_items[0]))
+		'''print(order_items)
+					items = order_items[0].item
+					print(items)'''
+		featured_items = Product.objects.filter(featured=True)
+		categories = Category.objects.all()
+		#capacities = Capacity.objects.all()
+		length = len(order_items)
+		
+		context = {
+			'order_items': order_items,
+			'order': order,
+			'length': length,
+			'featured_items': featured_items,
+			'categories': categories,
+			#'capacities': capacities
+			}
+
+	else:
+		featured_items = Product.objects.filter(featured=True)
+		categories = Category.objects.all()
+		length = 0
+
+		context = {
+			'length': length,
+			'featured_items': featured_items,
+			'categories': categories,
 		}
 
 	return render(request, "index.html", context)
@@ -35,33 +48,37 @@ def signup_view(request, *args, **kwargs):
 	return render(request, 'signup.html', {})
 
 def cart_view(request, *args, **kwargs):
-	order_check = Order.objects.filter(user=request.user,ordered=False)
-	no_order = ""
-	order_items = order_check[0].products.all()
-	order_total = 0
-	for item in order_items:
-		order_total += item.item.price*item.quantity
-
-	order_tax = float(order_total)*0.0825
-	order_tax = '%.2f'%order_tax
-
-	order_total_tax = float(order_total) + float(order_tax)
-	order_total_tax = '%.2f'%order_total_tax
-
-	if order_check.exists():
-		order = order_check
+	if request.user.is_authenticated:
+		order_check = Order.objects.filter(user=request.user,ordered=False)
+		no_order = ""
 		order_items = order_check[0].products.all()
+		order_total = 0
+		for item in order_items:
+			order_total += item.item.price*item.quantity
+
+		order_tax = float(order_total)*0.0825
+		order_tax = '%.2f'%order_tax
+
+		order_total_tax = float(order_total) + float(order_tax)
+		order_total_tax = '%.2f'%order_total_tax
+
+		if order_check.exists():
+			order = order_check
+			order_items = order_check[0].products.all()
+
+		else:
+			no_order = "no items to display"
+
+		context = {
+			'order_total': order_total,
+			'order_tax': order_tax,
+			'order_total_tax': order_total_tax,
+			'no_order': no_order,
+			'order': order,
+			'order_items': order_items,
+		}
+
+		return render(request, 'cart.html', context)
 
 	else:
-		no_order = "no items to display"
-
-	context = {
-		'order_total': order_total,
-		'order_tax': order_tax,
-		'order_total_tax': order_total_tax,
-		'no_order': no_order,
-		'order': order,
-		'order_items': order_items,
-	}
-
-	return render(request, 'cart.html', context)
+		return redirect('/signup')
